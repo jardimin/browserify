29b1a43e445da62357250dea68919717de4b69db
Jardins <contato@jardim.in>
2016-11-01 19:42:30 -0200

    SaibaMais

--- src/views/hipervideo-view-mobile.vue
+++ src/views/hipervideo-view-mobile.vue
@@ -34,7 +34,12 @@
       </div>
     </main>
     <div id="info" :style="{bottom: bottom_default + touch_slide + info_offset +'px'}" :class="[{touch: touch},{open: info_open}]">
-      <div class="handle" @touchstart.prevent="touchStart" @touchend.prevent="touchEnd" @touchmove.prevent="touchMove"><i class="material-icons">keyboard_arrow_up</i></div>
+      <div class="handle" @touchstart.prevent="touchStart" @touchend.prevent="touchEnd" @touchmove.prevent="touchMove" :class="{saiba: saiba_mais}">
+        <i class="material-icons">keyboard_arrow_up</i>
+        <transition name="saiba">
+          <div v-if="saiba_mais" class="saiba-mais">SAIBA MAIS</div>
+        </transition>
+      </div>
       <div class="body"></div>
     </div>
     <transition name="fade">
@@ -68,9 +73,11 @@ export default {
       touch: false,
       touch_point: 0,
       touch_slide: 0,
+      touch_time: 0,
       bottom_default: -480,
       info_open: false,
-      info_offset: 0
+      info_offset: 0,
+      saiba_mais: false
     }
   },
 
@@ -80,6 +87,23 @@ export default {
         this.$refs.engine.play()
       } else {
         this.$refs.engine.pause()
+      }
+    }, 
+    info_open: function (val, oldVal) {
+      if (val) {
+        this.info_offset = 480
+        this.saiba_mais = false
+      } else {
+        this.info_offset = 0
+      }
+    }, 
+    saiba_mais: function (val, oldVal) {
+      if (val) {
+        this.bottom_default = -460
+      } else {
+        setTimeout( () => {
+          this.bottom_default = -480
+        }, 300)
       }
     }
   },
@@ -109,9 +133,16 @@ export default {
       this.$parent.$parent.drawer = true
       console.log(this.$parent.drawer)
     },
+    showMais () {
+      this.saiba_mais = true
+      setTimeout( () => {
+        this.saiba_mais = false
+      },5000)
+    },
     touchStart (e) {
       this.touch = true
       this.touch_point = e.changedTouches[0].clientY
+      this.touch_time = e.timeStamp
     },
     touchMove (e) {
       this.touch_slide = -(e.changedTouches[0].clientY - this.touch_point)
@@ -120,10 +151,10 @@ export default {
       this.touch = false
       if ( this.touch_slide > 100 ) {
         this.info_open = true
-        this.info_offset = 480
       } else if ( this.touch_slide < -100 ) {
         this.info_open = false
-        this.info_offset = 0
+      } else if ( e.timeStamp - this.touch_time < 200 ) {
+        this.info_open = !this.info_open
       }
       this.touch_point = 0
       this.touch_slide = 0
@@ -144,7 +175,7 @@ export default {
 
   mounted: function () {
     this.$nextTick( () => {
-
+      
     })
   },
 
@@ -153,6 +184,9 @@ export default {
       componentHandler.upgradeDom()
       if (!this.video_load) {
         this.$refs.engine.load()
+        setTimeout( () => {
+          this.showMais()
+        }, 2000)
       }
     })
 
@@ -305,11 +339,37 @@ export default {
     width: 100%;
     box-shadow: 0 6px 10px 0 rgba(0,0,0,.14),0 1px 18px 0 rgba(0,0,0,.12),0 -3px 5px -1px rgba(0,0,0,.2);
     background: #D1C4E9;
+    &.saiba {
+      height: 45px;
+    }
     .material-icons {
       transition: transform .3s;
       margin-left: 45%;
       font-size: 40px;
       margin-top: -3px;
+    }
+    .saiba-mais {
+      height: 20px;
+      width: 100%;
+      font-size: 20px;
+      text-align: center;
+      margin-top: -12px;
+      padding-top: 1px;
+      font-weight: 900;
+      background: red;
+      color: white;
+      transition: background .2s, font-size .3s ease .2s, padding-top .3s ease .2s;
+      &.saiba-enter-active, &.saiba-leave.saiba-leave-active {
+        background: red;
+        font-size: 20px;
+        padding-top: 1px; 
+      }
+      &.saiba-leave-active, &.saiba-enter {
+        transition: background .2s ease .3s, font-size .3s, padding-top .3s;
+        background: transparent;
+        font-size: 0px;
+        padding-top: 5px;
+      }
     }
   }
 }
